a4ce964930fab7efd0daa87ad605db8b6dbe3b3f
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-12-12 14:16:38 +0400

    added new queris and commands in alarms module

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -2,10 +2,19 @@ import { DynamicModule, Module, Type } from '@nestjs/common';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
 import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmsService } from './alarms.service';
+import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
+import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 
 @Module({
   controllers: [AlarmsController],
-  providers: [AlarmsService, AlarmFactory],
+  providers: [
+    AlarmsService,
+    AlarmFactory,
+    CreateAlarmCommandHandler,
+    GetAlarmsQueryHandler,
+    AlarmCreatedEventHandler,
+  ],
 })
 export class AlarmsModule {
   static withInfrastucture(infrastructureModule: Type | DynamicModule) {
