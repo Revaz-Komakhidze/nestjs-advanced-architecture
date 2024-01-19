222b9b54f27996c3e0e31db7686418cd65a6486b
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-01-19 21:47:49 +0400

    created modules for shared services

---
+++ src/shared/infrastructure/shared-infrastructure.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EVENT_STORE_CONNECTION } from 'src/core/core.constants';
+import { EventDeserializer } from './event-store/desirializers/event.deserializer';
+import { EventsBridge } from './event-store/events-bridge';
+import { MongoEventStore } from './event-store/mongo-event-store';
+import { EventStorePublisher } from './event-store/publishers/event-store.publisher';
+import { EventSchema } from './event-store/schemas/event.schema';
+import { EventSerializer } from './event-store/serializers/event.serializer';
+import { EventStore } from './application/ports/event-store';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature(
+      [{ name: Event.name, schema: EventSchema }],
+      EVENT_STORE_CONNECTION,
+    ),
+  ],
+  providers: [
+    EventSerializer,
+    EventStorePublisher,
+    MongoEventStore,
+    EventsBridge,
+    EventDeserializer,
+    {
+      provide: EventStore,
+      useExisting: MongoEventStore,
+    },
+  ],
+  exports: [EventStore],
+})
+export class SharedInfrastructureModule {}
