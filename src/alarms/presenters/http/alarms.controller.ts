4af8b603cdc16e093eb2e0aa91d85a2e3f056dd5
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-02-06 21:47:42 +0400

    added alarm acknowledge endpoint

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { AlarmsService } from '../../application/alarms.service';
+import { CreateAlarmCommand } from '../../application/commands/create-alarm.command';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
-import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
 
 @Controller('alarms')
 export class AlarmsController {
@@ -22,5 +22,10 @@ export class AlarmsController {
   @Get()
   findAll() {
     return this.alarmsService.findAll();
+  }
+
+  @Patch(':id/acknowledge')
+  acknowledge(@Param('id') id: string) {
+    return this.alarmsService.acknowledge(id);
   }
 }
