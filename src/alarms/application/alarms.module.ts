92668c91930181f6d4e380c6fd16251de0b927bc
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-02-21 21:59:56 +0400

    wired up everything in application module

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -5,6 +5,11 @@ import { AlarmsService } from './alarms.service';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
+import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
+import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledged.event-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
+import { NotifyFacilitySupervisorCommandHandler } from './commands/notify-facility-supervisor.command-handler';
+import { UnacknowledgedAlarmsSaga } from './sagas/unacknowledged-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -14,6 +19,11 @@ import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-h
     CreateAlarmCommandHandler,
     GetAlarmsQueryHandler,
     AlarmCreatedEventHandler,
+    AcknowledgeAlarmCommandHandler,
+    AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
+    NotifyFacilitySupervisorCommandHandler,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
